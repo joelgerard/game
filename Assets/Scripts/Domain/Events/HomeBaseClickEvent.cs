052965b33ae8dd72fc861448c591ddb7ada134cc
Joel Gerard <joelgerard@gmail.com>
2019-09-13 13:17:19 -0700

    Renderer and domain are separate

---
+++ Assets/Scripts/Domain/Events/HomeBaseClickEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeBaseClickEvent : GameEvent
+{
+    public Vector2 pos;
+
+    public HomeBaseClickEvent(Vector2 pos)
+    {
+        this.pos = pos;
+    }
+}
